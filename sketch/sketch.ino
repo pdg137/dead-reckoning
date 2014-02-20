99e0ae29e5334642c1fab9d8a1cf552a5a570755
Paul Grayson <pdg@alum.mit.edu>
2014-02-20 15:52:41 -0800

    moved encoder 1 to INT pins

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,4 +1,4 @@
-uint8_t x = HIGH;
+  uint8_t x = HIGH;
 
 int count1 = 0;
 int count2 = 0;
@@ -8,29 +8,35 @@ uint8_t last11, last12, last21, last22;
 int error1 = 0;
 int error2 = 0;
 
-ISR(PCINT0_vect)
+// Encoder 1 uses INT0 and INT1
+ISR(INT1_vect,ISR_ALIASOF(INT0_vect));
+ISR(INT0_vect)
 {
-    digitalWrite(13, 1);
-    
-  uint8_t new11 = ((PINB & 0x10) != 0);
-  uint8_t new12 = ((PINB & 0x20) != 0);
-  uint8_t new21 = ((PINB & 0x40) != 0);
-  uint8_t new22 = ((PINB & 0x80) != 0);
+  uint8_t new11 = ((PIND & 0x01) != 0);
+  uint8_t new12 = ((PIND & 0x02) != 0);
   
   count1 += (last11 ^ new12) - (int)(new11 ^ last12);
-  count2 += (last21 ^ new22) - (int)(new21 ^ last22);
   
   if((last11 ^ new11) & (last12 ^ new12))
     error1 = 1;
+    
+  last11 = new11;
+  last12 = new12;
+}
+
+// Encoder two uses PCINT4 and PCINT7 (which trigger the PCINT0 interrupt)
+ISR(PCINT0_vect)
+{
+  uint8_t new21 = ((PINB & 0x10) != 0);
+  uint8_t new22 = ((PINB & 0x80) != 0);
+  
+  count2 += (last21 ^ new22) - (int)(new21 ^ last22);  
   
   if((last21 ^ new21) & (last22 ^ new22))
     error2 = 1;
   
-  last11 = new11;
-  last12 = new12;
   last21 = new21;
   last22 = new22;
-    digitalWrite(13, 0);
 }
 
 ISR(PCINT1_vect,ISR_ALIASOF(PCINT0_vect));
@@ -43,9 +49,14 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
   cli();
-  PCMSK0 = 0xf0; // enable pin change interrupts on PCINT4-7 which are Arduino pins 8-11, Port B 4-7
+  PCMSK0 = 0x90; // enable pin change interrupts on PCINT4 and 7 which are Arduino pins 8, 11, PB4 and PB7
   PCICR = 0xff; // turns on pin change interrupts in general
   PCIFR = 0; // clear interrupt flags
+  
+  EICRA = 0x05; // set INT0 and INT to interrupt on all edges
+  EIMSK = 0x03; // enable INT0 and INT1
+  EIFR = 0; // clear interrupt flags
+  
   sei();
 }
 
@@ -57,9 +68,7 @@ void loop() {
     Serial.println(count2);
     last_count1 = count1;
     last_count2 = count2;
-
   }
-  
   
   if(error1)
   {
