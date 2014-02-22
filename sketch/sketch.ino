cef176cf93e0a2545fae8dbdd7701889eb5a1593
Paul Grayson <pdg@alum.mit.edu>
2014-02-21 22:09:38 -0800

    encoders and motors working together - example that keeps the motors locked at their initial position

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,4 +1,4 @@
-  uint8_t x = HIGH;
+uint8_t x = HIGH;
 
 int count1 = 0;
 int count2 = 0;
@@ -24,7 +24,7 @@ ISR(INT0_vect)
   last12 = new12;
 }
 
-// Encoder two uses PCINT4 and PCINT7 (which trigger the PCINT0 interrupt)
+// Encoder two uses PCINT4 and PCINT7 (which trigger the PCINT0 interrupt)  
 ISR(PCINT0_vect)
 {
   uint8_t new21 = ((PINB & 0x10) != 0);
@@ -57,10 +57,46 @@ void setup() {
   EIMSK = 0x03; // enable INT0 and INT1
   EIFR = 0; // clear interrupt flags
   
+  // 20kHz PWM copied from Zumo shield library
+  TCCR1A = 0b10100000;
+  TCCR1B = 0b00010001;
+  ICR1 = 400;
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  
   sei();
 }
 
 void loop() {
+  
+  if(count1 > 0 && count1 <= 50)
+  {
+    digitalWrite(4, LOW);
+    OCR1A = count1*8;
+  }
+  else if(count1 < 0 && count1 >= -50)
+  {
+    digitalWrite(4, HIGH);
+    OCR1A = -count1*8;
+  }
+  else
+    OCR1A = 0;
+  
+  if(count2 > 0 && count2 <= 50)
+  {
+    digitalWrite(5, LOW);
+    OCR1B = count2*8;
+  }
+  else if(count2 < 0 && count2 >= -50)
+  {
+    digitalWrite(5, HIGH);
+    OCR1B = -count2*8;
+  }
+  else
+    OCR1B = 0;
+  
   if(last_count1 != count1 || last_count2 != count2)
   {
     Serial.print(count1);
